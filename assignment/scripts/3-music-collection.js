ed86da9f52a288450dceb935aecffe73d010accc
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-02-14 13:59:09 -0600

    code and implement stretch goal 1 (Search Function); update readme to check off tasks; general code cleanup, output cleanup, and commenting cleanup for readability; add ray charles albums for additional search function testing.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,6 +2,7 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+//addToCollection Function
 function addToCollection (title, artist, yearPublished) {
     let newAlbum = {
         albumTitle: title,
@@ -12,16 +13,19 @@ function addToCollection (title, artist, yearPublished) {
     return newAlbum;
 }
 
-
+console.log('--- Adding Music to Collection Start ---')
 console.log(addToCollection('Europop','Eiffel 65',1999))
 console.log(addToCollection('Texas Flood', 'Stevie Ray Vaughn', 1983))
 console.log(addToCollection('Le Pop', 'Katzenjammer', 2008))
 console.log(addToCollection('A Kiss Before You Go','Katzenjammer', 2011))
 console.log(addToCollection('Rockland','Katzenjammer',2015))
 console.log(addToCollection('Tonight Alright','Spiderbait',2004))
-console.log(collection)
+console.log(addToCollection('Unknown Album', 'Ray Charles', 1957))
+console.log(addToCollection('Unknown Album 2','Ray Charles', 1957))
+//console.log(collection)
 
 
+//showCollection Function
 function showCollection(array) {
     console.log("There are " + array.length + " items in the array.")
     for (album of array) {
@@ -29,21 +33,43 @@ function showCollection(array) {
     }
 }
 
-console.log(showCollection(collection));
+console.log('--- Show Collection Output Start ---')
+showCollection(collection);
 
 
+//findByArtist Function
 function findByArtist(artist) {
-    let searchArray = [];
+    let findByArtistArray = [];
     for (let i=0; i < collection.length; i++) {
         // testing only, clutters up output otherwise.
         //console.log('looking at', collection[i] );
         if (collection[i].albumArtist === artist) {
-            searchArray.push(collection[i]);
+            findByArtistArray.push(collection[i]);
         } 
-    } return searchArray;
+    } return findByArtistArray;
 } // end findByArtist
 //positioning of the [i]'s is very important and tricky!!
+console.log('--- findByArtist Output Start ---')
 
 console.log('Searching for Stevie Ray Vaugh', findByArtist('Stevie Ray Vaughn'));
 console.log('Searching for Katzenjammmer', findByArtist('Katzenjammer'));
 console.log('Searching for Billy Idol', findByArtist('Billy Idol'));
+
+
+
+// Stretch Goal 1
+function search(searchInput) {
+    let searchArray = [];
+    for (let i=0; i<collection.length; i++) {
+        if (searchInput.artist === collection[i].albumArtist && searchInput.year === collection[i].albumYearPublished) {
+            searchArray.push(collection[i]);
+            console.log[searchArray];
+        }
+    } return searchArray;
+} // end search
+
+console.log('--- Stretch Goal 1 ---')
+console.log('searching for requested information...', search({ artist: 'Ray Charles', year: 1957 })) // unclear why console is returning [object Object] when included
+// in the function with a plus (+), but returns an array when using a comma (,)
+console.log('searching for requested information...', search({ artist: 'Ray Charles', year: 1960 }))
+console.log('searching for requested information...', search({ artist: 'Ray Charles', year: 1957 }))
--- readme.md
+++ readme.md
@@ -48,15 +48,15 @@ X - Test the `findByArtist` function. Make sure to test with an artist you know 
 
 ### Stretch goals
 
-- Create a function called `search`. This function should:
-  - Take an input parameter for a search criteria object. Create your solution based on a search object that has these properties:
+X- Create a function called `search`. This function should:
+X  - Take an input parameter for a search criteria object. Create your solution based on a search object that has these properties:
   ```
   { artist: 'Ray Charles', year: 1957 }
   ```
-  - The returned output from `search` should meet these requirements:
-    - Return a new array of all items in the `collection` matching *all* of the search criteria.
-    - If no results are found, return an empty array.
-    - If there is no search object or an empty search object provided as input, then return all albums in the `collection`.
+X  - The returned output from `search` should meet these requirements:
+X    - Return a new array of all items in the `collection` matching *all* of the search criteria.
+X    - If no results are found, return an empty array.
+X    - If there is no search object or an empty search object provided as input, then return all albums in the `collection`.
 
 - Add an array of `tracks` to your album objects. Each track should have a `name` and `duration`. You will need to update the functions to support this new property:
   - Update the `addToCollection` function to also take an input parameter for the array of tracks.
