f98eefacf94d8542cb49e25c0add2222885b5c0b
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-02-13 18:23:33 -0600

    filled out addToCollection function with required processes.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,5 +3,12 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection (title, artist, yearPublished) {
-    
+    let newAlbum = {
+        albumTitle: title,
+        albumArtist: artist,
+        albumYearPublished: yearPublished
+    }
+    collection.push(newAlbum);
+    return newAlbum;
 }
+
