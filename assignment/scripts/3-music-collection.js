60f612ad46e1fb0a3d31cd988bd9e13c20247fa8
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-02-16 00:17:36 -0600

    updated for all stretch goals; search function only works with ideal data sets.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -21,6 +21,7 @@ function addToCollection (title, artist, yearPublished, trackArray) {
 }
 
 console.log('--- Adding Music to Collection Start ---')
+
 console.log(addToCollection('Europop','Eiffel 65',1999,europopTracks))
 console.log(addToCollection('Texas Flood', 'Stevie Ray Vaughn', 1983,texasFloodTracks))
 console.log(addToCollection('Le Pop', 'Katzenjammer', 2008))
@@ -30,7 +31,6 @@ console.log(addToCollection('Tonight Alright','Spiderbait',2004))
 console.log(addToCollection('Unknown Album', 'Ray Charles', 1957))
 console.log(addToCollection('Unknown Album 2','Ray Charles', 1959))
 //console.log(collection)
-
 
 //showCollection Function
 function showCollection(array) {
@@ -49,7 +49,6 @@ function showCollection(array) {
 
 console.log('--- Show Collection Output Start ---')
 showCollection(collection);
-
 //this was for tracking down individual tracks.
 //console.log('--- testing individual tracks start ---')
 //console.log(collection[0].tracks[0])
@@ -78,7 +77,64 @@ console.log('Searching for Billy Idol', findByArtist('Billy Idol')); // should o
 
 // Stretch Goal 1
 
+
+function search(searchInput) {
+    if (searchInput.tracks != undefined ) { // if tracks isn't undefined...
+        for (i = 0; i < collection.length; i++) { // iterate through collection...
+            if (collection[i].tracks === undefined ) { // but if an item lacks tracks -> return collection
+                // return collection;
+            } else {
+                for (x = 0; x < collection[i].tracks.length; x++) { // if a collection item has tracks..
+                    // console.log('x = ' + x) // testing output only
+                    if ( collection[i].tracks[x][0] === searchInput.tracks ) { //see if coollection's tracks match search query
+                        return collection[i].tracks[x][0]; // and return the matching item.
+                        } // if we put else return null here we get null if not the first track.
+                }
+            }
+        }
+    } else {
+    let results = collection.filter(obj => (obj.title === searchInput.title || !searchInput.title)
+    && (obj.artist === searchInput.artist || !searchInput.artist) 
+    && (obj.yearPublished === searchInput.year || !searchInput.year));
+    if (results.length > 0) {
+    return results;
+        } else {
+            return collection;
+        }
+    }
+}
+// most robust so far. !propertyName tells it to ignore undefined properties.
+// that filter thing ended up pretty long on a single line - hopefully this is the proper formatting for readability.
+
+console.log('--- Stretch Goal 1 ---')
+// annoyingly the search works, but not for the right reasons. It's just dumping out the collection once it iterates through the first two items.
+// presumably swapping them around with an entry with no tracks would cause it to stop working immediately. Update: it does.
+// would be nice to get this working elegantly but for the moment we can pretend we have a good and complete dataset.
+// or at least one that has dummy info in the tracks. Could probably write a function to do that.
+
+console.log('searching for requested information...')
+console.log(search({artist: 'Eiffel 65', tracks: 'Europop'})); // should return 1 item, 'Europop'. 
+console.log(search({artist: 'Eiffel 65', tracks: 'Too Much of Heaven'})); // should return 1 item, 'Too Much of Heaven'
+console.log(search({artist: 'Eiffel 65', tracks: 'Power'})); // should return collection. currently returns undefined.
+console.log(search({artist: 'Ray Charles', year: 1957 })) // should return 1 item
+console.log(search({artist: 'Ray Charles'})); // should return 2 items
+console.log(search({artist: 'Dave Matthews Band'})); // should return collection
+console.log(search({year: 1999})); // should return 1 item
+console.log(search({year: 2023})); // should return collection
+console.log(search({title: 'Le Pop'})); // should return 1 item
+console.log(search({title: 'Insomniac'})); // should return collection
+console.log(search({artist: 'Katzenjammer', year: 2011, title: 'A Kiss Before You Go'})) // should return 1 item
+console.log(search({tracks: 'Pride and Joy'})); // should return 1 item, 'Pride and Joy'
+
+//Stretch Goal 2
+//Inserted into above code!
+
 // -- INTERNAL NOTES - PLEASE IGNORE -- //
+
+//console.log(collection.tracks) // this produces undefined. 
+//console.log(collection[0].tracks[0][0]) // this produces too much of heaven, so clearly need to be specific. Can see the whole object when it is returned
+// but can't just log it into existence. Probably smart for ginormous data sets.
+
 // This works with the given search, but is not very flexible.
 //     for (let i=0; i<collection.length; i++) {
 //         if (searchInput.artist === collection[i].albumArtist && searchInput.year === collection[i].albumYearPublished) {
@@ -91,7 +147,7 @@ console.log('Searching for Billy Idol', findByArtist('Billy Idol')); // should o
 //         }
 //     } if (searchArray === [] ) {
 //         return collection;
-//     } else {
+//     } else { // this is where that else ended up.
 //         return searchArray;
 //     }
 // } // end search
@@ -107,33 +163,3 @@ console.log('Searching for Billy Idol', findByArtist('Billy Idol')); // should o
 //     })
 // }
 // -- END INTERNAL NOTES -- //
-
-// most robust so far. !propertyName tells it to ignore undefined properties.
-// that filter thing ended up pretty long on a single line - hopefully this is the proper formatting for readability.
-
-function search(searchInput) {
-    let results = collection.filter(obj => (obj.title === searchInput.title || !searchInput.title)
-    && (obj.artist === searchInput.artist || !searchInput.artist) 
-    && (obj.yearPublished === searchInput.year || !searchInput.year)
-    && (obj.tracks === searchInput.track || !searchInput.track)); // not currently working, always returns collection
-    if (results.length > 0) {
-        return results;
-    } else return collection;
-}    
-
-
-console.log('--- Stretch Goal 1 ---')
-
-console.log('searching for requested information...')
-console.log(search({artist: 'Ray Charles', year: 1957 })) // should return 1 item
-console.log(search({artist: 'Ray Charles'})); // should return 2 items
-console.log(search({artist: 'Dave Matthews Band'})); // should return collection
-console.log(search({year: 1999})); // should return 1 item
-console.log(search({year: 2023})); // should return collection
-console.log(search({title: 'Le Pop'})); // should return 1 item
-console.log(search({title: 'Insomniac'})); // should return collection
-console.log(search({artist: 'Katzenjammer', year: 2011, title: 'A Kiss Before You Go'})) // should return 1 item
-console.log(search({track: 'Pride and Joy'})); // should return 1 item
-
-//Stretch Goal 2
-//Inserted into above code!
--- readme.md
+++ readme.md
@@ -60,8 +60,8 @@ X    - If there is no search object or an empty search object provided as input,
 
 X - Add an array of `tracks` to your album objects. Each track should have a `name` and `duration`. You will need to update the functions to support this new property:
 X - Update the `addToCollection` function to also take an input parameter for the array of tracks.
-  - Update `search` to allow a `trackName` search criteria.
-    - If the search object has a `trackName` property, only search for that, ignoring any `artist` or `year` properties.
+X  - Update `search` to allow a `trackName` search criteria.
+X    - If the search object has a `trackName` property, only search for that, ignoring any `artist` or `year` properties.
 X  - Update the `showCollection` function to display the list of tracks for each album with its name and duration.
 ```
     TITLE by ARTIST, published in YEAR:
